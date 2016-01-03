3b6e4386d62c6ef4afb880c693ab02090dcfccb0
fuxin_home <goitt0519@gmail.com>
2016-01-03 23:31:10 +0800

    add pub include path to php include path

--- test/index.php
+++ test/index.php
@@ -1,9 +1,15 @@
 <?php
 
+$start_time = microtime(true);
+
 $now_path = dirname(__FILE__);
 $test_include_path = PATH_SEPARATOR.$now_path.'/../'.'functions';
+//设置公共pub库
 set_include_path(get_include_path().$test_include_path);
 include("pubfunc.php");
 
 pr(get_include_path());
 
+$end_time = microtime(true);
+$total_used_time = round($end_time - $start_time, 10);
+pr("\n总计耗时:{$total_used_time}s");
