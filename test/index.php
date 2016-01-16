ef62f84a8c8ff2e14ef664ad7faf6e7c1e37fcaf
fuxin_home <goitt0519@gmail.com>
2016-01-16 17:30:32 +0800

    test for unique id create

--- test/index.php
+++ test/index.php
@@ -7,8 +7,15 @@ $test_include_path = PATH_SEPARATOR.$now_path.'/../'.'functions';
 //设置公共pub库
 set_include_path(get_include_path().$test_include_path);
 include("pubfunc.php");
+include("pub.serial.php");
 
-pr(get_include_path());
+$id = makeSerialId(300);
+var_dump(extractVirtShardId($id));
+var_dump(isGlobalSerialId($id));
+var_dump(isValidSerialId($id));
+var_dump($id);
+echo "1";
+
 
 $end_time = microtime(true);
 $total_used_time = round($end_time - $start_time, 10);
